764cbc602ff5a5fbac2743591ea39cb14bd35192
mxaxel <axel.gustavo2001@outlook.com>
2022-02-11 22:35:58 +0000

    add progrma2.cpp

---
+++ programa2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+ 
+cout<< "inicia programa  " << fflush;
+cout<< "este es un texto " << endl; 
+cout<< "platano  " << "manzana " << "naranja "<<endl;
+cout<< "esto es mas texto" << endl;
+return 0;
+
+}
