d3d8ea3d56acecce9348c7d20647c0fc478096ed
autor <autor>
2022-02-21 02:49:39 +0000

    add programa3

---
+++ programa3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int numerogato = 5; 
+int numeroperro =7;
+cout<<"elnumero de gato es"<<numerogato<<endl;
+cout<<"el numero de perros es"<<numeroperro<<endl;
+
+int numerodn =numeroperro+1;
+cout<<"el nuevo numero de perros es "<<numerodn<<endl;
+return 0;
+}
